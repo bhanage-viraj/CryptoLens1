0a623ebaa1d7f057d6fbc6d2b1fbf1d0968330c4
bhanage-viraj <bhanagevirajvaibhav@gmail.com>
2025-02-09 21:40:43 +0530

    Update navbar.jsx

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = () => {
   const [account, setAccount] = useState('');
